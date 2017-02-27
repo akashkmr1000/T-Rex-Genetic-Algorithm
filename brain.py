5132c2350d17533abc1c042a061c30d32d853903
Tejan <tejank10@gmail.com>
2017-02-27 19:59:22 +0530

    Second Commit

--- brain.py
+++ brain.py
@@ -4,164 +4,120 @@ import numpy as np
 from pygame.locals import *
 import time
 from math import sqrt, exp, atan, pi
-
+import matplotlib.pyplot as plt
 screen = pygame.display.set_mode((600, 150), DOUBLEBUF)
 
-land = pygame.image.load('assets/landx.png')
+land = pygame.image.load('assets/land.png')
 cloud = pygame.image.load('assets/cloud.png')
 
+trex = {
+	'start': pygame.image.load('assets/start.png'),
+	'jump': pygame.image.load('assets/trex-jump.png'),
+	'run1': pygame.image.load('assets/trex-run1.png'),
+	'run2': pygame.image.load('assets/trex-run2.png'),
+	'duck1': pygame.image.load('assets/trex-duck1.png'),
+	'duck2': pygame.image.load('assets/trex-duck2.png'),
+	'over': pygame.image.load('assets/over.png')
+}
 start = pygame.image.load('assets/start.png')
-trex_jump=pygame.image.load('assets/trex-jump.png')
-trex_run1=pygame.image.load('assets/trex-run1.png')
-trex_run2=pygame.image.load('assets/trex-run2.png')
-over = pygame.image.load('assets/over.png')
 
-cactus_big_1=pygame.image.load('assets/cact1.png')
-cactus_small_1=pygame.image.load('assets/cact2.png')
+cact_big = [pygame.image.load('assets/cact_big'+str(i)+'.png') for i in range(1, 5, 1)]
+cact_small = [pygame.image.load('assets/cact_small'+str(i)+'.png') for i in range(1, 7, 1)]
+cluster = pygame.image.load('assets/cluster.png')
 
 game_over = pygame.image.load('assets/game_over.png')
 
-numbers=[]
-for i in range(10):
-	numbers.append(pygame.image.load('assets/numbers/'+str(i)+'.png'))
+numbers =[pygame.image.load('assets/numbers/'+str(i)+'.png') for i in range(10)]
 
 clock = pygame.time.Clock()
 
-SCREEN_HEIGHT=screen.get_height()
-SCREEN_WIDTH=screen.get_width()
-GROUND=SCREEN_HEIGHT-land.get_height()
-HEAD=GROUND-33
-XSHIFT=land.get_width()-SCREEN_WIDTH-4
-ACCLR=0.0001
-GRAVITY=0.0125
-JUMP_HEIGHT=62.168
-TREX_X=19
-CLOUD_SPEED=0.125
-MAX_SPEED=1.5
-FRAME_RATE=55000
-JUMP_SPEED=1.55
-#TIME_TAKEN in one iter = 0.000002 + 0.001
+SCREEN_HEIGHT = screen.get_height()
+SCREEN_WIDTH = screen.get_width()
+TREX_X = 19
+GROUND = SCREEN_HEIGHT-land.get_height()
+TREX_Y = SCREEN_HEIGHT-start.get_height()
+XSHIFT = land.get_width()-SCREEN_WIDTH-4
+ACCLR = 0.0001
+GRAVITY = 0.0125
+JUMP_Y = 9.687499999999707
+CLOUD_SPEED = 0.125
+MAX_SPEED = 1.4
+FRAME_RATE = 500
+JUMP_SPEED = 1.55
 
-cloud_info=[[SCREEN_WIDTH, random.randint(5, HEAD-5-cloud.get_height())]]
+graph=[]
+cloud_info = [[SCREEN_WIDTH, random.randint(5, TREX_Y-5-cloud.get_height())]]
 for i in range(3):
-	cloud_info.append([cloud_info[-1][0]+random.randint(50, SCREEN_WIDTH), random.randint(5, HEAD-5-cloud.get_height())])
+	cloud_info.append([cloud_info[-1][0]+random.randint(50, SCREEN_WIDTH), random.randint(5, TREX_Y-5-cloud.get_height())])
 
 
-class t_rex():
-	def __init__(self):
-		self.x=TREX_X
-		self.y=HEAD
-
-	def jump(self):
-		global score
-		height=HEAD
-		s=JUMP_SPEED
-		while s>0:
-			score+=1
-			environment()
-			s-=GRAVITY
-			height-=s
-			self.y=height
-			screen.blit(trex_jump, (self.x, self.y))
-			scorer()
-			if (isCollision(self.x, self.y)):
-				return True
-			pygame.display.flip()
-			clock.tick(FRAME_RATE)
-
-		scorer()
-
-		while s<JUMP_SPEED:
-			score+=1
-			environment()
-			s+=GRAVITY
-			height+=s
-			self.y=height
-			screen.blit(trex_jump, (self.x, self.y))
-			scorer()
-			if isCollision(self.x, self.y):
-				return True
-			pygame.display.flip()
-			clock.tick(FRAME_RATE)
+def jump(y, speed_Y):
+	global speed_X
+	if speed_Y>-JUMP_SPEED:
+		y-=speed_Y
+		speed_Y-=GRAVITY
+	screen.blit(trex['jump'], (TREX_X, y))
+	return y, speed_Y
 
 
-
-	def run(self):
-		global score
-		score+=1
-		environment()
-		screen.blit(trex_run1, (self.x, self.y))
-		pygame.display.flip()
-		clock.tick(FRAME_RATE)
-
-		environment()
-		screen.blit(trex_run2, (self.x, self.y))
-		scorer()
-		pygame.display.flip()
-		clock.tick(FRAME_RATE)
-		if isCollision(self.x, self.y):
-			return True
+def run(leftLeg):
+	if leftLeg == 1:
+		screen.blit(trex['run1'], (TREX_X, TREX_Y))
+	else:
+		screen.blit(trex['run2'], (TREX_X, TREX_Y))
 
 
 def preprocess():
-	global score, score2, landx, land2x, speed, cactii, cact_info, removed
+	global score, score2, landx, land2x, speed_X, cactii, cactii_y, cact_types, cact_big, cact_small, cluster, removed
+	removed=0
 
-	score=score2=landx=removed=0
+	score=score2=landx=0
 	land2x=-SCREEN_WIDTH
-	speed=0.5
-	#Types of Cactus
-	cactii=([HEAD+12, cactus_small_1], [HEAD-2, cactus_big_1])
+	speed_X=0.75
+	#Heights of Cactus
+	cactii_y=[SCREEN_HEIGHT-cact_small[0].get_height()+2, 2+SCREEN_HEIGHT-cact_big[0].get_height(),\
+	 2+SCREEN_HEIGHT-cluster.get_height()]
+	#Types of cactus
+	cact_types=[cact_small, cact_big, cluster]
 	#List of cactii
-	cact_info=[]
-	cact_info.append([SCREEN_WIDTH-landx,  cactii[random.randint(0, 1)]])
+	cactii=[[cact_types[1][0], random.randint(SCREEN_WIDTH, 1000), cactii_y[1]]]
+	makeCactii()
+
+
+def makeCactii():
+	global cactii, cactii_y, cact_types
 	for i in range(5):
-		k=random.randint(1,4)
-		for j in range(k):
-			if j == 0:
-				cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width()+random.randint(150+int(100*speed), SCREEN_WIDTH),\
-				 cactii[random.randint(0, 1)]])
-			else:
-				cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width(),  cactii[random.randint(0, 1)]])
+		no_of_cactii=random.randint(1,4)
+		if no_of_cactii != 4:
+			cact_=random.randint(0,1)
+			select_cact=random.sample(range(len(cact_types[cact_])), no_of_cactii)
+			for j in range(len(select_cact)):
+				if j==0:
+					cactii.append([cact_types[cact_][select_cact[j]], cactii[-1][1]+random.randint(300, 1000), cactii_y[cact_]])
+				else:
+					cactii.append([cact_types[cact_][select_cact[j]], cactii[-1][1]+cactii[-1][0].get_width(), cactii_y[cact_]])
+		else:
+			cact_=2
+			cactii.append([cact_types[cact_], cactii[-1][1]+random.randint(300, 1000), cactii_y[cact_]])
 
 
 def environment():
-		global landx, speed, land2x, cact_info, cactii, removed
+		global landx, speed_X, land2x, cactii, cact_types, cactii_y, removed
 		screen.fill((247, 247, 247))
 
 		if(cloud_info[0][0]+cloud.get_width()<0):
 			cloud_info.pop(0)
-			cloud_info.append([cloud_info[-1][0]+random.randint(50, SCREEN_WIDTH), random.randint(5, HEAD-5-cloud.get_height())])
+			cloud_info.append([cloud_info[-1][0]+random.randint(50, SCREEN_WIDTH), random.randint(5, TREX_Y-5-cloud.get_height())])
 
 		for i in range(len(cloud_info)):
 			screen.blit(cloud, (cloud_info[i][0], cloud_info[i][1]))
 			cloud_info[i][0]-=CLOUD_SPEED
 		#Updating list of cactii, removing the cactii passed out of screen and adding new one
-		to_be_removed=[]
-		for i in range(len(cact_info)):
-			if cact_info[i][0]+cact_info[i][1][1].get_width()<0:
-				to_be_removed.append(i)
-
-		for i in range(len(to_be_removed)-1, -1, -1):
-			cact_info.pop(to_be_removed[i])
-			if cact_info[to_be_removed[i]][1][1].get_width()+cact_info[to_be_removed[i]][0]<cact_info[to_be_removed[i]+1][0]:
-				removed+=1
-
-		while(len(cact_info)<20):
-			k=random.randint(1, 4)
-			for j in range(k):
-				if k<3:
-					if j == 0:
-						cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width()+random.randint(150+int(100*speed), SCREEN_WIDTH),\
-					 	cactii[random.randint(0, len(cactii)-1)]])
-					else:
-						cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width(),  cactii[random.randint(0, len(cactii)-1)]])
-				else:
-					if j == 0:
-						cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width()+random.randint(150+int(100*speed), SCREEN_WIDTH),\
-					 	cactii[random.randint(0, 2)%2]])
-					else:
-						cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width(),  cactii[random.randint(0, 2)%2]])
-
+		if cactii[0][1]+cactii[0][0].get_width()<0:
+			cactii.pop(0)
+			removed+=1
+		while(len(cactii)<20):
+			makeCactii()
 		
 		#printing land
 		screen.blit(land, (-landx, GROUND))
@@ -169,22 +125,24 @@ def environment():
 		#checking if land1 has shifted enough to introduce land2
 		if(landx>=XSHIFT):
 			screen.blit(land, (-land2x, GROUND))
-			land2x+=speed
+			land2x+=speed_X
 
 		#updating position of cactii
-		for i in range(len(cact_info)):
-			cact_info[i][0]-=speed
-			screen.blit(cact_info[i][1][1], (cact_info[i][0], cact_info[i][1][0]))
+		for i in range(len(cactii)):
+			cactii[i][1]-=speed_X
+			screen.blit(cactii[i][0], (cactii[i][1], cactii[i][2]))
 
 		#Replacing land by land2 after checking if land has completely gone out of screen
 		if(landx>land.get_width()):
 			landx=land2x
 			land2x=-SCREEN_WIDTH
 		#New land position
-		landx+=speed
+		landx+=speed_X
+
 
 def scorer():
-	global score,score2,speed
+	global score,score2
+	score+=speed_X
 	score2=int(score//25)
 	score_str=str(score2)
 	digits=[int(i) for i in score_str]
@@ -194,74 +152,116 @@ def scorer():
 		pos-=numbers[digits[i]].get_width()+5
 
 
-def isCollision(x, y):
-	global cact_info
-	for i in range(len(cact_info)):
-		if(x+3*trex_jump.get_width()/4<cact_info[i][0] or cact_info[i][0]+cact_info[i][1][1].get_width()<x+3*trex_jump.get_width()/4):
+def isCollision(y):
+	global cactii
+	x=TREX_X
+	for i in range(len(cactii)):
+		if(x+4*trex['jump'].get_width()/5<cactii[i][1] or cactii[i][1]+cactii[i][0].get_width()<x+trex['jump'].get_width()/5):
 			continue
-		elif (y+3*(trex_jump.get_height())/4<cact_info[i][1][0]):
+		elif (y+4*(trex['jump'].get_height())/5<cactii[i][2]):
 			continue
 		else:
 			return True
 	return False
 
 
-
 def game():
-	global score2, speed, removed
-	weights=[[np.array([np.array([random.uniform(-1, 1) for i in range(5)]) for j in range(4)]), np.array([random.uniform(-1, 1) for k in range(6)])] for l in range(10)]
-	fitness=[0 for i in range(10)]
+	global score2, speed_X, removed
+
+	weights=[[np.array([np.array([random.uniform(-1, 1) for i in range(5)]) for j in range(4)]), np.array([random.uniform(-1, 1)\
+	 for k in range(6)])] for l in range(50)]
+
 	max_score=0
-	for _ in range(200):
-		nn=[t_rex() for i in range(10)]
+	plt.ion()
+	plt.show()
+	for _ in range(1000):
+		#State of each genotype in population, whether alove or dead
+		popAlive= [1 for i in range(50)]
+		#Y coordinate of each genotype, indicates the jump state
+		popY= [TREX_Y for i in range(50)]
+		#speed in y direction for jumping
+		popJumpSpeed = [JUMP_SPEED for i in range (50)]
+
+		noOfJumps=[0 for i in range(50)]
+
+		#When population is dead
+		zeroPop=[0 for i in range(50)]	
+
+		fitness=[0 for i in range(50)]
+
 		score=score2=0
-		for i in range(10):
-			preprocess()
-			jumps=1
-			while not isCollision(nn[i].x, nn[i].y):
-				input_layer=make_input()
-				input_layer=np.array(input_layer)
-				if train_(input_layer, weights[i])>=0:
-					nn[i].jump()
-					jumps+=1
-				nn[i].run()
-				if(speed<MAX_SPEED):
-					speed+=ACCLR
-				if(score2>30000):
-					break
-				
-			fitness[i]=getFitness(score2, removed, jumps)
-			print('Epoch {_} Rex {i} Fitness {fit:.2f} cact jumped {cj} score {score2}'.format(_=_, i=i, score2=score2, fit=round(fitness[i], 2), cj=removed))
+		leftLeg=1
+
+		preprocess()
+
+		while popAlive != zeroPop:
 			environment()
-			screen.blit(over, (TREX_X, 300))
-			pygame.time.delay(100)
-			if(score2>max_score):
-				max_score=score2
-			if(score2>30000):
-				break
-		if(score2>30000):
-			break
-		weights, fitness=sortByFitness(weights,fitness)
+			for i in range(50):
+				if popAlive[i]==1:
+					#Check for jumping status
+					if popY[i] == TREX_Y: 	#If not in a jump
+						input_layer=makeInput()
+						input_layer=np.array(input_layer)
+
+						if train_(input_layer, weights[i])>=0:
+							popY[i], popJumpSpeed[i]=jump(popY[i], popJumpSpeed[i])
+							noOfJumps[i]+=1
+						else:
+							run(leftLeg)
+
+					else:	#If in a jump
+						popY[i], popJumpSpeed[i]=jump(popY[i], popJumpSpeed[i])
+						if popJumpSpeed[i]<=-JUMP_SPEED:
+							popY[i]=TREX_Y
+							popJumpSpeed[i]=JUMP_SPEED
+
+					if isCollision(popY[i]):
+						popAlive[i]=0
+						if noOfJumps[i]==0:
+							fitness[i]=0
+						else:
+							fitness[i]=removed/noOfJumps[i]
+
+					if(speed_X<MAX_SPEED):
+						speed_X+=ACCLR
+
+			scorer()
+			pygame.display.flip()
+			clock.tick(FRAME_RATE)
+			leftLeg=int(not leftLeg)
+
+		screen.blit(trex['over'], (TREX_X, 300))
+		if(score2>max_score):
+			max_score=score2
+			
+		print('Generation{_} Fitness {fit:.2f}'.format(_=_+1, fit=round(sum(fitness)/50, 2)))
+		graph.append(sum(fitness))
+		plt.plot(graph)
+		plt.ylabel('generation')
+		plt.xlabel('fitness')
+		plt.pause(0.1)
+		weights, fitness=sortByFitness(weights, fitness)
 		weights=crossoverMutate(weights)
-		weights=survive(weights)
 
 	print('MAX SCORE: ', max_score)
 
-def getFitness(score, removed, jumps):
-	return exp(-((removed/jumps-1)**2*2))#+atan(score-250)*2/pi
 
-def make_input():
-	global cact_info, speed
+def makeInput():
+	global cactii, speed_X, cact_types
 	#input=[BIAS, DISTANCE, NO_OF_CACTII, SPEED]
 	no_of_cactii=1
-	for i in range(1, len(cact_info)):
-		if cact_info[i][0]-cact_info[i-1][0]<=cact_info[i-1][1][1].get_width():
+	for i in range(1, len(cactii)):
+		if cactii[0][0].get_width()==cact_types[2].get_width():
+			no_of_cactii=4
+			break
+		elif cactii[i][1]-cactii[i-1][1]<=cactii[i-1][0].get_width():
 			no_of_cactii+=1
 		else:
 			break
 
-	input_vector=[1, (cact_info[0][0]-TREX_X-trex_run1.get_width())/(SCREEN_WIDTH-225), no_of_cactii/4, speed/MAX_SPEED]
+	input_vector=[1, (cactii[0][1]-TREX_X+trex['run1'].get_width())/(SCREEN_WIDTH-225), no_of_cactii/4, speed_X/MAX_SPEED]
 	return input_vector
+
 
 def train_(input_layer, weights):
 	hidden_layer=sigmoid(np.dot(input_layer, weights[0]))
@@ -269,22 +269,26 @@ def train_(input_layer, weights):
 
 	return output
 
+
 def sortByFitness(weights, fitness):
-	for x in range(9):
-		for y in range(9-x):
+	for x in range(49):
+		for y in range(49-x):
 			if fitness[y]<fitness[y+1]:
 				fitness[y], fitness[y+1], weights[y], weights[y+1] = fitness[y+1], fitness[y], weights[y+1], weights[y]
 	return weights, fitness
 
+
 def crossoverMutate(weights):
-	for i in range (3):
+	for i in range (25):
 		#CROSSOVER
 		co_weights0=random.sample(range(20), 8)
 		co_weights1=random.sample(range(6), 2)
 		rows=[k/5 for k in co_weights0]
 		cols=[k%5 for k in co_weights0]
 
-		weights[2*i-1][0][[rows, cols]], weights[2*i-1][1][co_weights1], weights[2*i][0][[rows, cols]], weights[2*i][1][co_weights1] = weights[2*i-1][0][[rows, cols]], weights[2*i-1][1][co_weights1], weights[2*i-1][0][[rows, cols]], weights[2*i-1][1][co_weights1]
+		weights[2*i-1][0][[rows, cols]], weights[2*i-1][1][co_weights1], weights[2*i][0][[rows, cols]],\
+		 weights[2*i][1][co_weights1] = weights[2*i-1][0][[rows, cols]], weights[2*i-1][1][co_weights1],\
+		  weights[2*i-1][0][[rows, cols]], weights[2*i-1][1][co_weights1]
 		
 		#MUTATION
 		mt_weights0=random.sample(range(20), 4)
@@ -299,9 +303,6 @@ def crossoverMutate(weights):
 
 	return weights
 
-def survive(weights):
-	weights[8:]=[[np.array([np.array([random.uniform(-1, 1) for i in range(5)]) for j in range(4)]), np.array([random.uniform(-1, 1) for k in range(6)])] for l in range(2)]
-	return weights
 
 def sigmoid(x):
 	return 1/(1+np.exp(-x))
--- game.py
+++ game.py
@@ -5,41 +5,43 @@ import time
 
 screen = pygame.display.set_mode((600, 150), DOUBLEBUF)
 
-land = pygame.image.load('assets/landx.png')
+land = pygame.image.load('assets/land.png')
 cloud = pygame.image.load('assets/cloud.png')
 
-start = pygame.image.load('assets/start.png')
-trex_jump=pygame.image.load('assets/trex-jump.png')
-trex_run1=pygame.image.load('assets/trex-run1.png')
-trex_run2=pygame.image.load('assets/trex-run2.png')
-over = pygame.image.load('assets/over.png')
 
-cactus_big_1=pygame.image.load('assets/cact1.png')
-cactus_small_1=pygame.image.load('assets/cact2.png')
+trex = {
+	'start': pygame.image.load('assets/start.png'),
+	'jump': pygame.image.load('assets/trex-jump.png'),
+	'run1': pygame.image.load('assets/trex-run1.png'),
+	'run2': pygame.image.load('assets/trex-run2.png'),
+	'duck1': pygame.image.load('assets/trex-duck1.png'),
+	'duck2': pygame.image.load('assets/trex-duck2.png'),
+	'over': pygame.image.load('assets/over.png')
+}
+
+cact_big = [pygame.image.load('assets/cact_big'+str(i)+'.png') for i in range(1, 5, 1)]
+cact_small = [pygame.image.load('assets/cact_small'+str(i)+'.png') for i in range(1, 7, 1)]
+cluster = pygame.image.load('assets/cluster.png')
 
 game_over = pygame.image.load('assets/game_over.png')
 
-numbers=[]
-for i in range(10):
-	numbers.append(pygame.image.load('assets/numbers/'+str(i)+'.png'))
+numbers =[pygame.image.load('assets/numbers/'+str(i)+'.png') for i in range(10)]
 
 clock = pygame.time.Clock()
 
-SCREEN_HEIGHT=screen.get_height()
-SCREEN_WIDTH=screen.get_width()
-GROUND=SCREEN_HEIGHT-land.get_height()
-HEAD=GROUND-33
-XSHIFT=land.get_width()-SCREEN_WIDTH-4
-ACCLR=0.0001
-GRAVITY=0.0125
-JUMP_HEIGHT=62.168
-TREX_X=19
-CLOUD_SPEED=0.125
-MAX_SPEED=1.4
-FRAME_RATE=500
-JUMP_SPEED=1.55
-#TIME_TAKEN in one iter = 0.000002 + 0.001
-
+SCREEN_HEIGHT = screen.get_height()
+SCREEN_WIDTH = screen.get_width()
+GROUND = SCREEN_HEIGHT-land.get_height()
+HEAD = SCREEN_HEIGHT-trex['start'].get_height()
+XSHIFT = land.get_width()-SCREEN_WIDTH-4
+ACCLR = 0.0001
+GRAVITY = 0.0125
+JUMP_HEIGHT = 62.168
+TREX_X = 19
+CLOUD_SPEED = 0.125
+MAX_SPEED = 1.4
+FRAME_RATE = 500
+JUMP_SPEED = 1.55
 cloud_info=[[SCREEN_WIDTH, random.randint(5, HEAD-5-cloud.get_height())]]
 for i in range(3):
 	cloud_info.append([cloud_info[-1][0]+random.randint(50, SCREEN_WIDTH), random.randint(5, HEAD-5-cloud.get_height())])
@@ -60,7 +62,7 @@ class t_rex():
 			s-=GRAVITY
 			height-=s
 			self.y=height
-			screen.blit(trex_jump, (self.x, self.y))
+			screen.blit(trex['jump'], (self.x, self.y))
 			scorer()
 			if (isCollision(self.x, self.y)):
 				return True
@@ -68,6 +70,7 @@ class t_rex():
 			clock.tick(FRAME_RATE)
 
 		scorer()
+		print(self.y)
 
 		while s<JUMP_SPEED:
 			score+=speed
@@ -75,7 +78,7 @@ class t_rex():
 			s+=GRAVITY
 			height+=s
 			self.y=height
-			screen.blit(trex_jump, (self.x, self.y))
+			screen.blit(trex['jump'], (self.x, self.y))
 			scorer()
 			if isCollision(self.x, self.y):
 				return True
@@ -88,12 +91,12 @@ class t_rex():
 		global score
 		score+=speed
 		environment()
-		screen.blit(trex_run1, (self.x, self.y))
+		screen.blit(trex['run1'], (self.x, self.y))
 		pygame.display.flip()
 		clock.tick(FRAME_RATE)
 
 		environment()
-		screen.blit(trex_run2, (self.x, self.y))
+		screen.blit(trex['run2'], (self.x, self.y))
 		scorer()
 		pygame.display.flip()
 		clock.tick(FRAME_RATE)
@@ -101,29 +104,57 @@ class t_rex():
 			return True
 
 
+	'''def duck(self):
+					global score
+					score+=speed
+					environment()
+					screen.blit(trex_duck1, (self.x, DUCK_HEAD))
+					pygame.display.flip()
+					clock.tick(FRAME_RATE)
+			
+					environment()
+					screen.blit(trex_duck2, (self.x, DUCK_HEAD))
+					scorer()
+					pygame.display.flip()
+					clock.tick(FRAME_RATE)
+					if isCollision(self.x, DUCK_HEAD):
+						return True
+			'''
+
+
 def preprocess():
-	global score, score2, landx, land2x, speed, cactii, cact_info
+	global score, score2, landx, land2x, speed, cactii, cactii_y, cact_types, cact_big, cact_small, cluster
 
 	score=score2=landx=0
 	land2x=-SCREEN_WIDTH
 	speed=0.75
-	#Types of Cactus
-	cactii=([HEAD+12, cactus_small_1], [HEAD-2, cactus_big_1])
+	#Heights of Cactus
+	cactii_y=[SCREEN_HEIGHT-cact_small[0].get_height()+2, SCREEN_HEIGHT-cact_big[0].get_height()+2,\
+	 2+SCREEN_HEIGHT-cluster.get_height()]
+	#Types of cactus
+	cact_types=[cact_small, cact_big, cluster]
 	#List of cactii
-	cact_info=[]
-	cact_info.append([SCREEN_WIDTH-landx,  cactii[random.randint(0, 1)]])
-	for i in range(5):
-		k=random.randint(1,4)
-		for j in range(k):
-			if j == 0:
-				cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width()+random.randint(150+int(100*speed), SCREEN_WIDTH),\
-				 cactii[random.randint(0, 1)]])
-			else:
-				cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width(),  cactii[random.randint(0, 1)]])
+	cactii=[[cact_types[1][0], random.randint(SCREEN_WIDTH, 1000), cactii_y[1]]]
+	make_cactii()
 
+def make_cactii():
+	global cactii, cactii_y, cact_types
+	for i in range(5):
+		no_of_cactii=random.randint(1,4)
+		if no_of_cactii<=3:
+			cact_=random.randint(0,1)
+			select_cact=random.sample(range(len(cact_types[cact_])), no_of_cactii)
+			for j in range(len(select_cact)):
+				if j==0:
+					cactii.append([cact_types[cact_][select_cact[j]], cactii[-1][1]+random.randint(300, 1000), cactii_y[cact_]])
+				else:
+					cactii.append([cact_types[cact_][select_cact[j]], cactii[-1][1]+cactii[-1][0].get_width(), cactii_y[cact_]])
+		else:
+			cact_=2
+			cactii.append([cact_types[cact_], cactii[-1][1]+random.randint(300, 1000), cactii_y[cact_]])
 
 def environment():
-		global landx, speed, land2x, cact_info, cactii
+		global landx, speed, land2x, cactii, cact_types, cactii_y
 		screen.fill((247, 247, 247))
 
 		if(cloud_info[0][0]+cloud.get_width()<0):
@@ -134,29 +165,10 @@ def environment():
 			screen.blit(cloud, (cloud_info[i][0], cloud_info[i][1]))
 			cloud_info[i][0]-=CLOUD_SPEED
 		#Updating list of cactii, removing the cactii passed out of screen and adding new one
-		to_be_removed=[]
-		for i in range(len(cact_info)):
-			if cact_info[i][0]+cact_info[i][1][1].get_width()<0:
-				to_be_removed.append(i)
-		for i in range(len(to_be_removed)-1, -1, -1):
-			cact_info.pop(to_be_removed[i])
-
-		while(len(cact_info)<20):
-			k=random.randint(1, 4)
-			for j in range(k):
-				if k<3:
-					if j == 0:
-						cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width()+random.randint(150+int(100*speed), SCREEN_WIDTH),\
-					 	cactii[random.randint(0, len(cactii)-1)]])
-					else:
-						cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width(),  cactii[random.randint(0, len(cactii)-1)]])
-				else:
-					if j == 0:
-						cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width()+random.randint(150+int(100*speed), SCREEN_WIDTH),\
-					 	cactii[random.randint(0, 2)%2]])
-					else:
-						cact_info.append([cact_info[-1][0]+cact_info[-1][1][1].get_width(),  cactii[random.randint(0, 2)%2]])
-
+		if cactii[0][1]+cactii[0][0].get_width()<0:
+			cactii.pop(0)
+		while(len(cactii)<10):
+			make_cactii()
 		
 		#printing land
 		screen.blit(land, (-landx, GROUND))
@@ -167,9 +179,9 @@ def environment():
 			land2x+=speed
 
 		#updating position of cactii
-		for i in range(len(cact_info)):
-			cact_info[i][0]-=speed
-			screen.blit(cact_info[i][1][1], (cact_info[i][0], cact_info[i][1][0]))
+		for i in range(len(cactii)):
+			cactii[i][1]-=speed
+			screen.blit(cactii[i][0], (cactii[i][1], cactii[i][2]))
 
 		#Replacing land by land2 after checking if land has completely gone out of screen
 		if(landx>land.get_width()):
@@ -190,11 +202,11 @@ def scorer():
 
 
 def isCollision(x, y):
-	global cact_info
-	for i in range(len(cact_info)):
-		if(x+3*trex_jump.get_width()/4<cact_info[i][0] or cact_info[i][0]+cact_info[i][1][1].get_width()<x+3*trex_jump.get_width()/4):
+	global cactii
+	for i in range(len(cactii)):
+		if(x+4*trex['jump'].get_width()/5<cactii[i][1] or cactii[i][1]+cactii[i][0].get_width()<x+trex['jump'].get_width()/5):
 			continue
-		elif (y+3*(trex_jump.get_height())/4<cact_info[i][1][0]):
+		elif (y+4*(trex['jump'].get_height())/5<cactii[i][2]):
 			continue
 		else:
 			return True
@@ -203,6 +215,9 @@ def isCollision(x, y):
 
 
 def game():
+	preprocess()
+	environment()
+	global landx
 	trex=t_rex()
 	global score, score2, speed
 	score=score2=0
@@ -211,53 +226,59 @@ def game():
 			if event.type==KEYDOWN:
 				if event.key==K_SPACE or event.key==K_UP:
 					trex.jump()
-				#elif event.key==K_DOWN:
-					#duck()
 		trex.run()
 		if(speed<MAX_SPEED):
 			speed+=ACCLR
 
 	environment()
-	screen.blit(over, (trex.x, trex.y+2))
-	return False
+	screen.blit(trex['over'], (trex.x, trex.y+2))
+	game_over_screen()
+	
 
 def start_game():
-	preprocess()
-	environment()
-	global landx
-	screen.blit(start, (TREX_X, HEAD))
+	global score
+	screen.fill((247, 247, 247))
+	screen.blit(trex['start'], (TREX_X, HEAD))
 	pygame.display.flip()
 	t=True
 	while t:
 		for event in pygame.event.get():
 			if(event.type == KEYDOWN):
 				if  event.key==K_SPACE:
-					global score
-					height=HEAD
-					s=JUMP_SPEED
-					while s>0:
-						s-=GRAVITY
-						height-=s
-						screen.blit(land, (0, GROUND))
-						screen.blit(trex_jump, (TREX_X, height))
-						pygame.display.flip()
-						clock.tick(FRAME_RATE)
+					t=False
 
-					while s<JUMP_SPEED:
-						s+=GRAVITY
-						screen.blit(land, (0, GROUND))
-						height+=s
-						screen.blit(trex_jump, (TREX_X, height))
-						pygame.display.flip()
-						clock.tick(FRAME_RATE)
+	height=HEAD
+	s=JUMP_SPEED
+	while s>0:
+		s-=GRAVITY
+		height-=s
+		screen.blit(land, (0, GROUND))
+		screen.blit(trex['jump'], (TREX_X, height))
+		pygame.display.flip()
+		clock.tick(FRAME_RATE)
+	while s<JUMP_SPEED:
+		s+=GRAVITY
+		screen.blit(land, (0, GROUND))
+		height+=s
+		screen.blit(trex['jump'], (TREX_X, height))
+		pygame.display.flip()
+		clock.tick(FRAME_RATE)
+	game()
 
-					t=game()
-					break
-				break
-			break
-
+def game_over_screen():
 	scorer()
 	screen.blit(game_over, (SCREEN_WIDTH/2-game_over.get_width()/2, 50))
+	screen.blit(restart, (SCREEN_WIDTH/2-restart.get_width()/2, 75))
 	pygame.display.flip()
-	pygame.time.delay(1000)
+	t=True
+	while t:
+		for event in pygame.event.get():
+			if(event.type == KEYDOWN):
+				if  event.key==K_SPACE:
+					t=False
+					game()
+				elif event.key==K_ESCAPE:
+					t=False
+		
+
 start_game()
